21f3f41f14c84ae4f3a4a10af82cbf1377025dcb
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2020-09-15 14:29:57 -0700

    new colorize script

--- colorize.sh
+++ colorize.sh
@@ -15,13 +15,15 @@ bits=$(seq 10 18)
 nthreads=95
 num_concurrent_datasets=1
 total_parallelism=$(( $nthreads * $num_concurrent_datasets))
-dst_s3="s3://sisu-datasets/unbiased-svms/"
+dst_s3="s3://sisu-datasets/glauber/"
 datasets="url kdda"
 
 echo "compressing $datasets across bits $(echo $bits)"
 
 for dataset in $datasets ; do
 
+echo "pre-processing dataset $dataset"
+    
 for i in $bits ; do 
     budget=$((1 << $i))
 flags="--k 10 --glauber-samples 10000000 "
@@ -34,26 +36,24 @@ echo \
   --train ./svms-data/${dataset}.train.svm \
   --valid ./svms-data/${dataset}.test.svm $flags
 done
-done
-done | shuf | RAYON_NUM_THREADS=$nthreads xargs -P $num_concurrent_datasets -L 1  ./csl/target/release/csl >svms-data/log-colorize.txt
+done | RAYON_NUM_THREADS=$nthreads xargs -P $num_concurrent_datasets -L 1  ./csl/target/release/csl >svms-data/log-colorize.txt
 
-# echo "parallel taring $dataset into ./svms-data/${dataset}.tar.zst"
+echo "parallel taring $dataset into ./svms-data/${dataset}.tar.zst"
 
-# tar -I "pzstd -p $total_parallelism" -cf ./svms-data/${dataset}.tar.zst \
-#   ./svms-data/${dataset}.{train,test}.svm \
-#   ./svms-data/${dataset}.{train,test}.{sm,ft}*.svm
+tar -I "pzstd -p $total_parallelism" -cf ./svms-data/${dataset}.tar.zst \
+   ./svms-data/${dataset}.{train,test}.svm \
+   ./svms-data/${dataset}.{train,test}.{sm,ft}*.svm
   
-# echo "uploading to s3"
-# aws s3 cp ./svms-data/${dataset}.tar.zst "$dst_s3${dataset}.tar.zst"
-# for t in train test ; do for s in sm ft ; do
-# cp ./svms-data/${dataset}.${t}.${s}1024.svm ./svms-data/saved${dataset}.${t}.${s}1024.svm
-# done ; done
-# rm ./svms-data/${dataset}.{train,test}.{un,ft}*.svm \
-#    ./svms-data/${dataset}.{train,test}.{ft,ht}*.svm.field_dims.txt
-  
-#done # dataset
+echo "uploading to s3"
+aws s3 cp ./svms-data/${dataset}.tar.zst "$dst_s3${dataset}.tar.zst"
+for t in train test ; do for s in sm ft ; do
+  cp ./svms-data/${dataset}.${t}.${s}1024.svm ./svms-data/saved${dataset}.${t}.${s}1024.svm
+done ; done
 
-exit 0
+rm ./svms-data/${dataset}.{train,test}.{sm,ft}*.svm \
+   ./svms-data/${dataset}.{train,test}.{ft,ht}*.svm.field_dims.txt
+  
+done # dataset
 
 budget=1024
 for dataset in $datasets ; do
@@ -61,15 +61,14 @@ for t in train test ; do for s in sm ft ; do
 mv ./svms-data/saved${dataset}.${t}.${s}1024.svm ./svms-data/${dataset}.${t}.${s}1024.svm
 done ; done
 
-echo "target encoding all datasets"
+# echo "target encoding all datasets"
 
-for dataset in $datasets ; do
-echo \
-  --budget 1024 --compress TargetEncode \
-  --train ./svms-data/${dataset}.train.svm \
-  --valid ./svms-data/${dataset}.test.svm
-done | RAYON_NUM_THREADS=$nthreads xargs -P $num_concurrent_datasets -L 1 ./csl/target/release/csl >/dev/null
-
+# for dataset in $datasets ; do
+# echo \
+#   --budget 1024 --compress TargetEncode \
+#   --train ./svms-data/${dataset}.train.svm \
+#   --valid ./svms-data/${dataset}.test.svm
+# done | RAYON_NUM_THREADS=$nthreads xargs -P $num_concurrent_datasets -L 1 ./csl/target/release/csl >/dev/null
 
 echo "binary-encoding all datasets"
 
@@ -98,14 +97,14 @@ echo "parallel taring all binary datasets into ./svms-data/binary.tar.zst"
 tar -I "pzstd -p $total_parallelism" -cf ./svms-data/binary.tar.zst \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{data,indices,indptr,y}.bin \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.svm \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,te,ft,ht}1024.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,te,ft}1024.svm
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.{data,indices,indptr,y}.bin \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft}1024.svm
 
 echo "uploading to s3"
 aws s3 cp ./svms-data/binary.tar.zst "${dst_s3}binary.tar.zst"
 
 rm \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,te,ft,ht}1024.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,te,ft,ht}1024.svm \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,te,ft,ht}1024.svm.field_dims.txt
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.{data,indices,indptr,y}.bin \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.svm \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.svm.field_dims.txt
