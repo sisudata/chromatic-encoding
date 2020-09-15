165b98d653a5e308514740e253c57ca3247033a4
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2020-09-15 23:26:35 +0000

    diff order num

--- colorize.sh
+++ colorize.sh
@@ -42,23 +42,23 @@ echo "parallel taring $dataset into ./svms-data/${dataset}.tar.zst"
 
 tar -I "pzstd -p $total_parallelism" -cf ./svms-data/${dataset}.tar.zst \
    ./svms-data/${dataset}.{train,test}.svm \
-   ./svms-data/${dataset}.{train,test}.{sm,ft}*.svm
+   ./svms-data/${dataset}.{train,test}.*{sm,ft}.svm
   
 echo "uploading to s3"
 aws s3 cp ./svms-data/${dataset}.tar.zst "$dst_s3${dataset}.tar.zst"
 for t in train test ; do for s in sm ft ; do
-  cp ./svms-data/${dataset}.${t}.${s}1024.svm ./svms-data/saved${dataset}.${t}.${s}1024.svm
+  cp ./svms-data/${dataset}.${t}.1024${s}.svm ./svms-data/saved${dataset}.${t}.1024${s}.svm
 done ; done
 
-rm ./svms-data/${dataset}.{train,test}.{sm,ft}*.svm \
-   ./svms-data/${dataset}.{train,test}.{ft,ht}*.svm.field_dims.txt
+rm ./svms-data/${dataset}.{train,test}.*{sm,ft}.svm \
+   ./svms-data/${dataset}.{train,test}.*{ft,ht,sm}.svm.field_dims.txt
   
 done # dataset
 
 budget=1024
 for dataset in $datasets ; do
 for t in train test ; do for s in sm ft ; do
-mv ./svms-data/saved${dataset}.${t}.${s}1024.svm ./svms-data/${dataset}.${t}.${s}1024.svm
+mv ./svms-data/saved${dataset}.${t}.1024${s}.svm ./svms-data/${dataset}.${t}.1024${s}.svm
 done ; done
 
 # echo "target encoding all datasets"
@@ -74,7 +74,7 @@ echo "binary-encoding all datasets"
 
 for dataset in $datasets ; do
 for suffix in ft sm te ; do
-suffix=".${suffix}${budget}"
+suffix=".${budget}${suffix}"
 
 echo ./svms-data/${dataset}.train${suffix}.svm
 echo ./svms-data/${dataset}.test${suffix}.svm
@@ -97,14 +97,14 @@ echo "parallel taring all binary datasets into ./svms-data/binary.tar.zst"
 tar -I "pzstd -p $total_parallelism" -cf ./svms-data/binary.tar.zst \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{data,indices,indptr,y}.bin \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.svm \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft}1024.svm
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.1024{sm,ft,ht}.{data,indices,indptr,y}.bin \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.1024{sm,ft}.svm
 
 echo "uploading to s3"
 aws s3 cp ./svms-data/binary.tar.zst "${dst_s3}binary.tar.zst"
 
 rm \
   ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.{data,indices,indptr,y}.bin \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.svm \
-  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.{sm,ft,ht}1024.svm.field_dims.txt
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.1024{sm,ft,ht}.{data,indices,indptr,y}.bin \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.1024{sm,ft,ht}.svm \
+  ./svms-data/{url,kdda,kddb,kdd12}.{train,test}.1024{sm,ft,ht}.svm.field_dims.txt
