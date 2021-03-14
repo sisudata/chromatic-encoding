8111f49cd0cf5f8579d8062960163e3c68cb5be8
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2021-03-14 09:16:01 -0700

    add subgraph functionality

--- crank/src/graph.rs
+++ crank/src/graph.rs
@@ -1,15 +1,9 @@
 //! Compact graph data structure.
 
-
-
-use rayon::iter::{ParallelIterator};
-
-
-
-
-
+use std::convert::TryInto;
 
 use rayon::iter::IndexedParallelIterator;
+use rayon::iter::ParallelIterator;
 use rayon::slice::ParallelSlice;
 
 pub(crate) type Vertex = u32;
@@ -58,5 +52,72 @@ impl Graph {
 
     pub fn nvertices(&self) -> usize {
         self.offsets.len() - 1
+    }
+    pub fn subgraph(&self, ixs: &[u32]) -> Self {
+        assert!(ixs.windows(2).all(|s| s[0] < s[1]));
+        assert!(ixs
+            .last()
+            .iter()
+            .all(|ix| (**ix as usize) < self.nvertices()));
+
+        let mut remap = vec![u32::MAX; self.nvertices()];
+        for (i, ix) in ixs.iter().copied().enumerate() {
+            remap[ix as usize] = i.try_into().unwrap();
+        }
+        let mut new_offsets = vec![0usize; ixs.len() + 1];
+        for &ix in ixs {
+            let down_ix = remap[ix as usize] as usize;
+            new_offsets[down_ix + 1] += self
+                .neighbors(ix)
+                .iter()
+                .filter(|nbr| remap[**nbr as usize] != u32::MAX)
+                .count();
+        }
+        cumsum_inplace(&mut new_offsets);
+
+        let mut new_neighbors = vec![u32::MAX; *new_offsets.last().expect("last")];
+        for &ix in ixs {
+            let down_ix = remap[ix as usize] as usize;
+            let (lo, hi) = (new_offsets[down_ix], new_offsets[down_ix + 1]);
+            let new_nbr_cnt = self
+                .neighbors(ix)
+                .iter()
+                .map(|ix| remap[*ix as usize])
+                .filter(|ix| *ix != u32::MAX)
+                .count();
+            assert!(hi - lo == new_nbr_cnt);
+            for (new_nbr, old_nbr) in new_neighbors[lo..hi].iter_mut().zip(
+                self.neighbors(ix)
+                    .iter()
+                    .map(|ix| remap[*ix as usize])
+                    .filter(|ix| *ix != u32::MAX),
+            ) {
+                *new_nbr = old_nbr;
+            }
+        }
+
+        let ret = Self {
+            offsets: new_offsets,
+            neighbors: new_neighbors,
+        };
+
+        // println!(
+        //     "{}",
+        //     json!({
+        //         "orig_degrees": SummaryStats::from(ixs.iter().map(|ix| self.degree(*ix) as f64)).to_map(),
+        //         "subgraph_degrees": SummaryStats::from((0..ixs.len()).map(|ix| ret.degree(ix as u32) as f64)).to_map(),
+        //     }
+        //     )
+        // );
+
+        ret
+    }
+}
+
+fn cumsum_inplace(x: &mut [usize]) {
+    let mut cumsum = 0;
+    for v in x.iter_mut() {
+        cumsum += *v;
+        *v = cumsum;
     }
 }
